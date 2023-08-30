513a832eaaef83b084c51443881b568d9ab1ae8a
HemantNavlani <hemant.navlani.0506@gmail.com>
2023-08-30 08:36:29 +0530

    Tic Tac Toe Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Grid from './components/Grid/Grid.jsx'
+
+function App() {
+
+  return (
+    <div>
+        <Grid numberOfCards={9}/>
+        </div>
+  )
+}
+
+export default App
